cbce9b01613d07cb0b4b575b93c28efa5096978a
Tim He <the@kns.com>
2018-05-09 19:42:20 +0800

    understand the route

--- react/src/index.js
+++ react/src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.less';
 import App from './common/App';
 import UserLayout from './common/UserLayout';
-
 import registerServiceWorker from './registerServiceWorker';
+// import "ant-design-pro/dist/ant-design-pro.less"
+import 'antd/dist/antd.less';
+import "ant-design-pro/dist/ant-design-pro.less";
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<UserLayout />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<UserLayout />, document.getElementById('root'));
 registerServiceWorker();
